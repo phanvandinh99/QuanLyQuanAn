19f189d7af69b60fd962ce0c530053ffbf3fa1ce
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2024-09-15 18:48:46 +0700

    Fix bug

--- DoAn/QuanLyNhaHang/Common/Const/Const.cs
+++ DoAn/QuanLyNhaHang/Common/Const/Const.cs
@@ -11,5 +11,10 @@
 
         public const int DaThanhToan = 0;
         public const int ChuaThanhToan = 1;
+
+        public const int SupperAdmin = 1;
+        public const int Admin = 2;
+        public const int Employee = 3;
+
     }
 }
--- DoAn/QuanLyNhaHang/Models/DoanhNghiep.cs
+++ DoAn/QuanLyNhaHang/Models/DoanhNghiep.cs
@@ -1,42 +1,31 @@
 namespace QuanLyNhaHang.Models
 {
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("DoanhNghiep")]
     public partial class DoanhNghiep
     {
         [Key]
-        [Column(Order = 0)]
         [StringLength(50)]
         public string MaDoanhNghiep { get; set; }
 
-        [Key]
-        [Column(Order = 1)]
         [StringLength(200)]
         public string TenDoanhNghiep { get; set; }
 
-        [Key]
-        [Column(Order = 2)]
         [StringLength(10)]
         public string SoDienThoai { get; set; }
 
         [StringLength(100)]
         public string Email { get; set; }
 
-        [Key]
-        [Column(Order = 3)]
         [StringLength(255)]
         public string DiaChi { get; set; }
 
-        [Key]
-        [Column(Order = 4)]
         public DateTime ThoiGianBatDau { get; set; }
 
-        public DateTime? ThoiGianKetThuc { get; set; }
+        public DateTime ThoiGianKetThuc { get; set; }
 
         public double? TongTien { get; set; }
     }
